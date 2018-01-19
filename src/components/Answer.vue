68347852cf767711bf654eff04df6c6b2c5df485
weizhengda <2625785357@qq.com>
2018-01-19 15:11:46 +0800

    1.19

---
+++ src/components/Answer.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="answer">
+      <h3>中国大陆的国际区号是？</h3>
+      <ul>
+          <li>+86</li>
+          <li>+61</li>
+          <li>+886</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Answer',
+  data(){
+    return{
+        app:'',
+    }
+  }
+}
+</script>
+<style scope>
+    #answer{
+        width:100%;
+        height:30%;
+        position:absolute;
+        background:#fefefe;
+    }
+    #answer:after{
+        width: 140%;
+        height: 100px;
+        position: absolute;
+        left: -20%;
+        top:100%;
+        z-index: 0;
+        content: '';
+        border-radius: 0 0 50% 50%;
+        background:#fff;
+    }
+</style>
--- src/views/Live.vue
+++ src/views/Live.vue
@@ -1,10 +1,12 @@
 <template>
-   <div class="home">
+   <div class="live">
        <player></player>
+       <answer></answer>
    </div>
 </template>
 <script>
-import Player from '@/components/player.vue'
+import Player from '@/components/Player.vue'
+import Answer from '@/components/Answer.vue'
 export default {
       data() {
         return {
@@ -12,7 +14,13 @@ export default {
         }
       },
       components:{
-          'player': Player
+          'player': Player,
+          'answer': Answer
       }
 }
 </script>
+<style scope>
+     .live{
+         overflow-x:hidden;
+     }
+</style>
